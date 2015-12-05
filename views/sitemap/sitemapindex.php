466088bf0c225871223d1e9a66f7d5941dcc4c25
Roumen Damianoff <roumen@dawebs.com>
2015-12-05 17:14:15 +0300

    added sitemapindex view

---
+++ views/sitemap/sitemapindex.php
@@ -0,0 +1,12 @@
+<?php header("Content-type: text/xml; charset=utf-8"); ?>
+<?= '<?xml version="1.0" encoding="UTF-8"?>'."\n"; ?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<?php foreach($sitemaps as $sitemap) : ?>
+	<sitemap>
+		<loc><?= $sitemap['loc'] ?></loc>
+	<?php if($sitemap['lastmod'] !== null) : ?>
+		<lastmod><?= date('Y-m-d\TH:i:sP', strtotime($sitemap['lastmod'])) ?></lastmod>
+	<?php endif; ?>
+	</sitemap>
+<?php endforeach; ?>
+</sitemapindex>
